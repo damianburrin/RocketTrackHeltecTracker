231bd7d1cb475b398c0ccc1e02e8bac3be64346d
Chris Eilbeck <chris@hyperspace.org.uk>
2022-07-25 11:46:51 +0100

    Tinkering with the build script for the Feather M0, working out what libraries I'll need to use the SPI flash for file storage.

--- RocketTrack/upload_to_featherM0.sh
+++ RocketTrack/upload_to_featherM0.sh
@@ -5,8 +5,8 @@
 	--verbose \
 	--fqbn adafruit:samd:adafruit_feather_m0
 
-python "/home/chris/.arduino15/packages/esp32/tools/esptool_py/3.0.0/esptool.py" \
-	--chip esp32 \
-	--port /dev/ttyUSB0 \
-	--baud 921600 \
-	write_flash -z 0x290000 rockettrack.spiffs.bin
+#python "/home/chris/.arduino15/packages/esp32/tools/esptool_py/3.0.0/esptool.py" \
+#	--chip esp32 \
+#	--port /dev/ttyUSB0 \
+#	--baud 921600 \
+#	write_flash -z 0x290000 rockettrack.spiffs.bin
